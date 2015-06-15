151b896367420b49a7abae2fb5ae3463a19f68ed
Ferran Palicio <ferran.palicio@gmail.com>
2015-06-15 18:53:08 +0200

    [WIP] Add new layout for kit detail screen
    Scrollview + LinearLayout (vertical)

--- app/src/main/java/cat/lafosca/smartcitizen/ui/activities/KitDetailActivity.java
+++ app/src/main/java/cat/lafosca/smartcitizen/ui/activities/KitDetailActivity.java
@@ -1,12 +1,8 @@
 package cat.lafosca.smartcitizen.ui.activities;
 
-import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 
 import cat.lafosca.smartcitizen.R;
 
@@ -15,27 +11,13 @@ public class KitDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_kit_detail_coordinator);
+        setContentView(R.layout.activity_kit_detail);
 
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(getString(R.string.kit_detail_title));
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        CollapsingToolbarLayout collapsingToolbar =
-                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
-        collapsingToolbar.setTitle("hello?");
-
-        CoordinatorLayout cl = (CoordinatorLayout)findViewById(R.id.main_content);
-
-        AppBarLayout appBar = (AppBarLayout)findViewById(R.id.appbar);
-        appBar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
-            @Override
-            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
-                Log.i("lol", "verticalOffset " + verticalOffset);
-            }
-        });
-
-        CollapsingToolbarLayout colToolbLayour = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
 
     }
 }
