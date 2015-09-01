8e577edf65af6e6cf06e9950c65f9821080b01a6
Ferran Palicio <ferran.palicio@gmail.com>
2015-09-01 15:21:29 +0200

    Add crashlytics

--- app/src/main/java/cat/lafosca/smartcitizen/commons/SmartCitizenApp.java
+++ app/src/main/java/cat/lafosca/smartcitizen/commons/SmartCitizenApp.java
@@ -3,6 +3,8 @@ package cat.lafosca.smartcitizen.commons;
 import android.app.Application;
 
 import cat.lafosca.smartcitizen.managers.SharedPreferencesManager;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by ferran on 04/06/15.
@@ -14,6 +16,7 @@ public class SmartCitizenApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         instance = this;
 
